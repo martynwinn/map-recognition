2993862017f1aeb3a1930a8034c1901f23e80fb0
martyn.winn@stfc.ac.uk <martyn.winn@stfc.ac.uk>
2018-07-09 16:49:45 +0100

    A few more explanatory comments.

--- extract_EM_slices.py
+++ extract_EM_slices.py
@@ -29,7 +29,7 @@ index_file = 'EM_slices_dataset_blurred_1axis.idx'
 
 ### options
 #np.set_printoptions(threshold=np.inf)
-# list of axes to section along
+# list of axes to section along, typically ['Z'] for 1axis or ['X','Y','Z'] for 3axes
 axes_list = ['Z']
 # linear dimension of slice
 window_size = 48
@@ -68,7 +68,10 @@ if not os.path.isdir(dirout):
 
 indexfile = open(index_file,'w')
 
-# loop over map data block 3 times, each time sectioning along different axis
+# Loop over map data block 3 times, each time sectioning along different axis.
+# The map data block is copied to a temporary array loc_mrcdata, and we use
+# np.transpose to change the axis order, i.e. if axis is 'X' then that is
+# the 3rd dimension, and slices are in the y-z plane.
 for axis in axes_list:
   if axis in ['X']:
     print("transposing")
@@ -105,12 +108,21 @@ for axis in axes_list:
             if n_ref_dens > window_size*window_size*good_fraction:
                 annotate = 'g_'
 
+            # So far, myslice has the original map values. By default, the output images
+            # are 8-bit, so we need to put these into the range 0 - 255. We use np.clip
+            # so that a given range of map values map into 0 - 255. Lower values are set
+            # to 0 and higher values set to 255. We can vary this, to enhance the features
+            # in the image (might also be done with data augmentation routines from Keras).
             if normalise:
                 myslice = 255.0 * np.clip((myslice - map_min) / (map_max - map_min),0,1)
 
             ### pillow
+            # output the 2D slices as standard PNG files, for input to the machine learning
             imgout_filename = annotate + '2984_bl_'+str(section)+'_'+str(col)+'_'+str(row)+axis+'.png'
             img_out = pimg.fromarray(myslice)
+            # Write as RGB ... it is really greyscale, so red=green=blue at every pixel.
+            # This must be wasteful, but the ML I started with assumed RGB and it was
+            # easiest to go with it.
             img_new = img_out.convert('RGB')
             img_new.save(dirout + '/' + imgout_filename)
 
